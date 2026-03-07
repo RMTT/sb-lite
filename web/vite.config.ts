41c49729b00a3faacdaf6b0353f1f6e412edebc6
RMT <iamrmttt@gmail.com>
2026-03-08 01:03:42 +0800

    fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+            'codemirror-vendor': [
+            '@uiw/react-codemirror',
+            '@codemirror/lang-json',
+            '@codemirror/theme-one-dark'
+          ]
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
